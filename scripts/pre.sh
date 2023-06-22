f3f22572d8a12ca92ab4aa34be38711b2547161b
Scott Harley <scott.harley@unity3d.com>
2023-06-22 18:41:57 -0400

    Explore the VM space a bit

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -1,6 +1,15 @@
 echo "Script start"
 pwd
+
+echo "Display contents of root dir"
 ls -alsR /
-ls "C:\Users\buildbot\.gradle\.gsdk_plugin"
-ls "/Users/buildbot/.gradle/.gsdk_plugin"
+
+echo "Display contents of $ANDROID_HOME"
+ls -alsR $ANDROID_HOME
+
+echo "Display contents of $ANDROID_NDK_ROOT"
+ls -alsR $ANDROID_NDK_ROOT
+
+# ls "C:\Users\buildbot\.gradle\.gsdk_plugin"
+# ls "/Users/buildbot/.gradle/.gsdk_plugin"
 echo "Script end"
