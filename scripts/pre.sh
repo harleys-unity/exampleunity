7185c94c29b67774ad90e4cae489955b3468de0e
Scott Harley <scott.harley@unity3d.com>
2023-06-22 19:27:41 -0400

    Adjusted commands to be less log-intensive

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -2,13 +2,13 @@ echo "Script start"
 pwd
 
 echo "Display contents of root dir"
-ls -alsR /
+ls -als /
 
 echo "Display contents of $ANDROID_HOME"
-ls -alsR $ANDROID_HOME
+ls -als $ANDROID_HOME
 
 echo "Display contents of $ANDROID_NDK_ROOT"
-ls -alsR $ANDROID_NDK_ROOT
+ls -als $ANDROID_NDK_ROOT
 
 # ls "C:\Users\buildbot\.gradle\.gsdk_plugin"
 # ls "/Users/buildbot/.gradle/.gsdk_plugin"
