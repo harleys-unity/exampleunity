22660e8f9d48830b31dee710a19e1e1eb15cd010
Scott Harley <scott.harley@unity3d.com>
2023-11-29 22:10:48 -0500

    Silly test for pre-script

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -1,23 +1,6 @@
 #!/bin/sh
 
-# echo "Script start"
-# pwd
-
-# echo "Display contents of root dir"
-# ls -als /
-
-# echo "Display contents of $ANDROID_HOME"
-# ls -als $ANDROID_HOME
-
-# echo "Display contents of $ANDROID_NDK_ROOT"
-# ls -als $ANDROID_NDK_ROOT
-
-# # ls "C:\Users\buildbot\.gradle\.gsdk_plugin"
-# # ls "/Users/buildbot/.gradle/.gsdk_plugin"
-# echo "Script end"
-
 file=test.txt
-
 echo "We can write files via Bash" > $file
 value=`cat $file`
 echo "TADA! $value"
